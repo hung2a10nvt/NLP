5c25cf787e5b07d0a3564f142312b4fd6c70afe9
hung2a10nvt <hung2a10@gmail.com>
2025-11-23 16:08:18 +0700

    Fix

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -11,13 +11,19 @@ def ensure_nltk_data():
             nltk.download(package)
 
 def process_text(file_path):    
-    with open(file_path, 'r', encoding='utf-8') as f:
-        text_content = f.read()
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            text_content = f.read()
+    except FileNotFoundError:
+        print(f"Error: Can't find {file_path}")
+        return []
 
     sentences = sent_tokenize(text_content, language='russian')
     morph = pymorphy3.MorphAnalyzer()
     matched_pairs = set()
     
+    TARGET_POS = {'NOUN', 'ADJF', 'PRTF'} 
+
     for sentence in sentences:
         tokens = word_tokenize(sentence.lower(), language='russian')
         words = [t for t in tokens if re.match(r"^[а-яё-]+$", t)]
@@ -26,22 +32,29 @@ def process_text(file_path):
             p1 = morph.parse(w1)[0]
             p2 = morph.parse(w2)[0]
             
-            pos1, pos2 = p1.tag.POS, p2.tag.POS
+            tag1, tag2 = p1.tag, p2.tag
             
-            # Noun/ Adjective pairs
-            if pos1 in {'NOUN', 'ADJF'} and pos2 in {'NOUN', 'ADJF'}:
-                gender1, gender2 = p1.tag.gender, p2.tag.gender
-                number1, number2 = p1.tag.number, p2.tag.number
-                case1, case2 = p1.tag.case, p2.tag.case
+            # Check if is Noun/Adj/Participle
+            if tag1.POS in TARGET_POS and tag2.POS in TARGET_POS:
                 
-                # Check for matching gender, number, and case
-                if (gender1 and gender2 and gender1 == gender2 and
-                    number1 and number2 and number1 == number2 and
-                    case1 and case2 and case1 == case2):
-                    pair = f"{p1.normal_form} {p2.normal_form}"
-                    matched_pairs.add(pair)
+                # Number
+                if tag1.number != tag2.number or not tag1.number:
+                    continue
+                
+                # Case
+                if tag1.case != tag2.case or not tag1.case:
+                    continue
+                
+                # Gender
+                # if adj's number = plur, pymorphy returns gender=None 
+                if tag1.number == 'sing':
+                    if tag1.gender != tag2.gender or not tag1.gender:
+                        continue
+                
+                pair = f"{p1.normal_form} {p2.normal_form}"
+                matched_pairs.add(pair)
     
-    return sorted(matched_pairs)
+    return sorted(list(matched_pairs))
 
 def main():
     ensure_nltk_data()
@@ -49,11 +62,11 @@ def main():
     results = process_text(input_file)
 
     if results:
-        print("Найденные пары:\n")
+        print(f"Found {len(results)} pairs:\n")
         for pair in results:
             print(pair)
     else:
-        print("Совпадающие пары не найдены.")
+        print("No pair found.")
 
 if __name__ == "__main__":
     main()
